1a0cfedd9409d350bd38b07f405ea7882d758a9b
Chris-Denmark <cdenmark97@gmail.com>
2021-03-04 14:15:45 -0600

    initial commit

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,55 @@
-// Put your code here
+const main = () => {
+  console.log("My Enemies List!")
+  console.log("----------------")
+  const enemies = [
+    {
+      firstName: "Joshua",
+      lastName: "Flowers",
+      offenses: {
+        firstOffense: "Being a jerk to me in elementary school",
+        secondOffense: "Not being nice to me in elementary school"
+      },
+      isReallyHated = true
+    },
+    {
+      firstName: "Darth",
+      lastName: "Vader",
+      offenses: {
+        firstOffense: "Cut off Luke's hand",
+        secondOffense: "Murdered all those kids",
+        thirdOffense: "Unkind management practices"
+      },
+      isReallyHated: false
+    },
+    {
+      firstName: "Betty",
+      lastName: "Rudelady",
+      offenses: {
+        firstOffense: "Phone calls in the theater",
+        secondOffense: "Phone calls on the bus",
+        thirdOffense: "Phone calls in line at the grocery store",
+        fourthOffense: "Poor conversationalist"
+      },
+      isReallyHated: true
+    },
+    {
+      firstName: "Leon",
+      lastName: "Peck",
+      offenses: {
+        firstOffense: "Keeps giving me a hotplate"
+      },
+      isReallyHated: false
+    }
+  ]
+
+
+  enemies.forEach(enemy => {
+    if (enemy.IsReallyHated) {
+      console.log(`${enemy.FirstName} ${enemy.LastName} (Really, really dislike!)`)
+    } else {
+      console.log(`${enemy.FirstName} ${enemy.LastName}`)
+    }
+  });
+}
+
+main()
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,17 @@
-// Put your code here
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+
+names.forEach(name => {
+  console.log(name)
+});
+
+console.log("")
+
+const theNames = names.filter(n => n.startsWith("The"))
+
+console.log("'The' Place Names")
+
+theNames.forEach(name => {
+  console.log(name)
+})
